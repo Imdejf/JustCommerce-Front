3aed71ea31204a6a8cdcb79415277d3dd3146de0
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2023-07-04 11:26:01 +0200

    Added formkit config

---
+++ formkit.config.ts
@@ -0,0 +1,9 @@
+import { pl } from '@formkit/i18n'
+import { DefaultConfigOptions } from '@formkit/vue'
+import '@formkit/themes/genesis'
+const config: DefaultConfigOptions = {
+  locales: { pl },
+  locale: 'pl',
+}
+
+export default config
