85a284a6f3427169e4606fc8d0c3048ad92e6338
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2023-07-04 09:25:47 +0200

    Test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-// tailwind.config.js
-const FormKitVariants = require('@formkit/themes/tailwindcss')
-
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  content: [
-    './src/**/*.{html,js,vue}',
-    './node_modules/@formkit/themes/dist/tailwindcss/genesis/index.cjs'
-  ],
-  darkMode: false, // or 'media' or 'class'
+  content: [],
   theme: {
     extend: {}
   },
-  variants: {
-    extend: {}
-  },
-  plugins: [FormKitVariants]
+  plugins: []
 }
